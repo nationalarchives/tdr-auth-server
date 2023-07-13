9a506f7fe663fdd1a99cb850461c5c8b9cd9a3a4
TomJKing <tom.king@nationalarchives.gov.uk>
2023-07-13 15:05:34 +0100

    try specifiying java version on action

--- event-publisher-spi/src/main/scala/uk/gov/nationalarchives/eventpublisherspi/UserMonitoringTask.scala
+++ event-publisher-spi/src/main/scala/uk/gov/nationalarchives/eventpublisherspi/UserMonitoringTask.scala
@@ -29,7 +29,7 @@ class UserMonitoringTask(snsClient: SnsClient,
       val users: List[UserModel] = userProvider.searchForUserStream(realm, userSearchParams).iterator().asScala.toList
 
       val usersNoMFA = users
-        .filter(u => Option(u.getServiceAccountClientLink).getOrElse("").isBlank)
+        .filter(u => Option(u.getServiceAccountClientLink).getOrElse("").isEmpty)
         .filter(u => {
           val credentialManager: SubjectCredentialManager = u.credentialManager()
           validConfiguredCredentialTypes.forall(credentialType => !credentialManager.isConfiguredFor(credentialType))
