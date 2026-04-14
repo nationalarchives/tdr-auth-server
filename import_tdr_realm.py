b30b2113521c9ee846877f6f4f5eeb571780ec25
thanhz <thanhz@users.noreply.github.com>
2026-04-14 12:12:38 +0100

    Update import_tdr_realm.py
    Wiz recommendation
    
    Co-authored-by: wiz-2986343e2e[bot] <248871866+wiz-2986343e2e[bot]@users.noreply.github.com>

--- import_tdr_realm.py
+++ import_tdr_realm.py
@@ -8,4 +8,4 @@ env_properties_file = os.environ['KEYCLOAK_CONFIGURATION_PROPERTIES']
 host = os.environ['KEYCLOAK_HOST']
 
 update_realm_configuration('/keycloak-configuration/', env_properties_file)
-subprocess.call(['/opt/keycloak/bin/kc.sh', '-v', 'start', '--server-async-bootstrap=false'])
+subprocess.run(['/opt/keycloak/bin/kc.sh', '-v', 'start', '--server-async-bootstrap=false'], check=True)
